b1228c9f3a6e3f8708e6362150fabe38f9a512d0
Hamid Zare <dellydela@gmail.com>
2019-10-29 17:20:37 -0700

    cleanup script

---
+++ bin/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm $MAIN_FILE.glo
+rm $MAIN_FILE.acn
+rm $MAIN_FILE.out
+rm $MAIN_FILE.aux
+rm $MAIN_FILE.lot
+rm $MAIN_FILE.toc
+rm $MAIN_FILE.lof
+rm $MAIN_FILE.blg
+rm $MAIN_FILE.bbl
